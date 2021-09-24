b12a4888d48ece53a5207db39fc388b1606c6bce
Jonathan C <jsea@uw.edu>
2021-09-24 12:59:02 -0700

    Added assignment files

---
+++ dom-structure.js
@@ -0,0 +1,19 @@
+// 1) Select each element in the DOM, and console log the element selection
+
+// body
+console.log();
+
+// h1
+console.log();
+
+// p
+console.log();
+
+// div
+console.log();
+
+// link to YouTube
+console.log();
+
+// link to Twitch
+console.log();
